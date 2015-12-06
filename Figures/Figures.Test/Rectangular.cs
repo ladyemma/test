99fa9b4398aad2d996690e7cd7eb8faf0b1a3383
ladyemma <surgutskaya.tatyana@gmail.com>
2015-12-06 22:40:43 +0300

    test project

---
+++ Figures/Figures.Test/Rectangular.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Figures.Test
+{
+    [TestClass]
+    public class Rectangular
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FirstArgumentExceptionTest()
+        {
+            Triangle.Rectangular.Area(0, new Random().Next(1, 100), new Random().Next(1, 100));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SecondArgumentExceptionTest()
+        {
+            Triangle.Rectangular.Area(new Random().Next(1, 100), 0, new Random().Next(1, 100));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void HypotenuseArgumentExceptionTest()
+        {
+            Triangle.Rectangular.Area(new Random().Next(1, 100), new Random().Next(1, 100), 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PositiveHypotenuseArgumentExceptionTest()
+        {
+            Triangle.Rectangular.Area(new Random().Next(1, 100), new Random().Next(1, 100), 5);
+        }
+        [TestMethod]
+        public void AreaByCatetusTest()
+        {
+            var a = new Random().Next(1, 100);
+            var b = new Random().Next(1, 100);
+            var h = Math.Sqrt(a * a + b * b);
+
+            var s = Triangle.Rectangular.Area(a, b, h);
+            var s2 = 0.5 * a * b;
+
+            Assert.AreEqual(Math.Round(s2, 2), Math.Round(s, 2));
+        }
+
+        [TestMethod]
+        public void AreaBySidesTest()
+        {
+            var a = new Random().Next(1, 100);
+            var b = new Random().Next(1, 100);
+            var h = Math.Sqrt(a*a+b*b);
+
+            var p = (a + b + h) / 2;
+            var s = (p - a) * (p - b);
+
+            Assert.AreEqual(Triangle.Rectangular.Area(a, b, h), s);
+        }
+    }
+}
---
+++ Figures/Triangle/Rectangular.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    public static class Rectangular
+    {
+        public static double Area(double cathetusA, double cathetusB, double hypotenuse)
+        {
+            if (cathetusA <= 0)
+                throw new ArgumentException("cathetusA");
+
+            if (cathetusB <= 0)
+                throw new ArgumentException("cathetusB");
+
+            if (hypotenuse <= 0)
+                throw new ArgumentException("hypotenuse");
+
+            if (hypotenuse.CompareTo(Math.Sqrt(cathetusA * cathetusA + cathetusB * cathetusB)) != 0)
+                throw new ArgumentException("square of the hypotenuse equals the sum of the squares of the legs");
+
+            var p = (cathetusA + cathetusB + hypotenuse) / 2;
+
+            return (p - cathetusA) * (p - cathetusB);
+        }
+    }
+}
